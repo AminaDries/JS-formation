b56d190f362f9317060f06619bcb40f18b80699e
AminaDries <aminadries25@gmail.com>
2024-03-28 13:29:12 +0100

    edit tab

---
+++ tableau/tableau2.js
@@ -0,0 +1,26 @@
+const age=[2, 15 , 8 , 65, 47 , 74]
+
+let result = age.filter(function(item){
+    return item >18
+})
+console.log(result);
+
+document.writeln(result)
+
+let somme = age.reduce(function(total, value){
+    return total + value
+
+}, 5)
+document.writeln(somme)
+
+let majeur= age.every(function(value, index, array){
+    return value >1
+})
+console.log(majeur)
+
+let names= ["Amina" , "Massiz"]
+let names2 = [...names] //créer une copie d'un tableau val tab1= val tab2
+names2.push("Tesnim")
+
+console.log(names, "names");
+console.log(names2, "names2");
